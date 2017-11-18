1fec8c0b8690f1d15258a48620d7582e48bc43b2
Jason Rowley <jasonrowley@verizon.net>
2017-11-18 14:32:28 -0500

    bfs implemented

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ from itertools import chain
 from math import sqrt
 from random import choice
 from vertex import Vertex
+from queue import Queue
 
 
 class Graph:
@@ -10,6 +11,33 @@ class Graph:
         self.search_type = search_type
         self.start_vertex = start_vertex
         self.goal_vertices = goal_vertices
+        self.path_cost = 0
+        self.path = []
+
+    def bfs(self):
+        print('bfs')
+        vertices = self.vertices
+        for k, v in vertices.items():
+            v.info()
+        queue = Queue()
+        queue.put(vertices['S'])
+        self.path.append('S')
+        while not queue.empty():
+            curr = queue.get()
+            i = 0
+            while (i < len(curr.edges)):
+                adjacent = curr.edges[i]
+                if not vertices[adjacent[0]].visited and vertices[adjacent[0]] not in self.goal_vertices:
+                    self.path_cost += curr.edges[i][1]
+                    vertices[adjacent[0]].visited = True
+                    queue.put(vertices[adjacent[0]])
+                    self.path.append(adjacent[0])
+                else:
+                    return
+                i+=1
+
+
+
 
 def parse_file(filename):
     print('gets to parse')
@@ -32,7 +60,7 @@ def parse_file(filename):
     for k, v in vertices.items():
         print(k, v)
         v.info()
-    return None
+    return vertices
 
 if __name__ == '__main__':
     import argparse
@@ -44,5 +72,11 @@ if __name__ == '__main__':
     parser.add_argument('start_state', default='S', help='Vertex to start search from')
     args = parser.parse_args()
 
-    parse_file(args.graph_file)
+    goal_vertices = ['G1', 'G2']
+    vertice_dict = parse_file(args.graph_file)
+
+
+    graph = Graph(vertice_dict, args.search, args.start_state, goal_vertices)
+
+    graph.bfs()
 
